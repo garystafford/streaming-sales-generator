ccbb5c26965c6cdbdea0bc856d264240df5ae347
Gary A. Stafford <gary.a.stafford@gmail.com>
2022-12-27 09:57:54 -0500

    Update Pinot script and notebook

--- apache_pinot_examples/pinot_sample_script.py
+++ apache_pinot_examples/pinot_sample_script.py
@@ -1,23 +1,31 @@
+# Apache Pinot Example
+# Purpose: Reads the contents of a realtime table from Apache Pinot containing streaming data
+# Author:  Gary A. Stafford
+# Date: 2022-12-27
+
 from pinotdb import connect
 
 conn = connect(host="pinot-broker", port=8099, path="/query/sql", scheme="http")
+
+sql = """
+    SELECT
+      product_id,
+      product_name,
+      product_category,
+      SUMPRECISION(purchase_quantity, 10, 0) AS quantity,
+      SUMPRECISION(total_purchase, 10, 2) AS sales
+    FROM
+      purchasesEnriched
+    GROUP BY
+      product_id,
+      product_name,
+      product_category
+    ORDER BY
+      sales DESC;"""
+
 curs = conn.cursor()
-curs.execute(
-    """
-SELECT
-  product_id,
-  product_name,
-  product_category,
-  SUMPRECISION(purchase_quantity, 10, 2) AS quantity,
-  SUMPRECISION(total_purchase, 10, 2) AS sales
-FROM
-  purchasesEnriched
-GROUP BY
-  product_id,
-  product_name,
-  product_category
-ORDER BY
-  sales DESC;"""
-)
+
+curs.execute(sql)
+
 for row in curs:
     print(row)
